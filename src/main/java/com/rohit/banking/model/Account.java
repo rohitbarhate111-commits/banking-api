33fbd15ee7a20651da560c8f34d6bc17e6cfb56e
rohiiittt <rohitbarhate111@gmail.com>
2026-03-21 13:30:39 +0530

    feat: add Account entity model

---
+++ src/main/java/com/rohit/banking/model/Account.java
@@ -0,0 +1,18 @@
+package com.rohit.banking.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accountHolderName;
+    private double balance;
+}
